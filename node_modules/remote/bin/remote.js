48cd58c3b83ac39d67e45908617f5f65bb929caf
Preston Stosur-Bassett <preston@stosur.info>
2016-02-03 17:43:52 -0500

    removed gitignore files that were accidentally uploaded

--- node_modules/remote/bin/remote.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-(function() {
-    require('coffee-script');
-    // Call the main function
-    var Remote = require('../libs/remote.coffee');
-		new Remote({cli:true});
-}).call(this);
