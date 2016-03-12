4c2bd3bd56b20866066c83193ef2fed2b0450e33
Preston Stosur-Bassett <preston@stosur.info>
2016-03-12 02:08:39 -0500

    Updated enging to support instant pinyin search without tone numbers. Fixed issue #4

--- src/node-cc-cedict/node-cc-cedict.js
+++ src/node-cc-cedict/node-cc-cedict.js
@@ -65,51 +65,92 @@ if(tradHashtable.isEmpty() || simpHashtable.isEmpty()) {
 			var pinyinIsEmpty = pinyinHashtable.isEmpty();
 
 			for(var i = 0; i < wordList.length; i++) {
-				if(tradIsEmpty == true) {
-					/*
-					* 	TODO: Change pinyin (wordList.pronunciation) to use tone marks instead of tone numbers
-					*/
+				if(pinyinIsEmpty == true) {
+					function runPinyin() {
+						'use strict'
+						let word = {
+							simplified: wordList[i].simplified,
+							traditional: wordList[i].traditional,
+							pronunciation: wordList[i].pronunciation,
+							definitions: wordList[i].definitions
+						};
 
-					if(tradHashtable.containsKey(wordList[i].traditional) == false) {
-						var addWord = [];
-						addWord.push(wordList[i]);
-						tradHashtable.put(wordList[i].traditional, addWord);
+						// Searchable pinyin without tone numbers
+						let searchablePinyin = word.pronunciation.substring(1, word.pronunciation.length - 1).replace(/[0-9]/g, '').replace(/\s+/g, '');
+
+						// Prettify the pinyin to use tone marks instead of tone numbers
+						word.pronunciation = pinyin.prettify(word.pronunciation.substring(1, word.pronunciation.length - 1));
+
+						// Add the word to the hashtable, if the character already exists in the hashtable as a key, add the second definition to the same key
+						if(pinyinHashtable.containsKey(searchablePinyin) == false) {
+							let addWord = [];
+							addWord.push(word);
+							pinyinHashtable.put(searchablePinyin, addWord);
+						}
+						else {
+							let addWord = pinyinHashtable.get(searchablePinyin);
+							addWord.push(word);
+							pinyinHashtable.put(searchablePinyin, addWord);
+						}
 					}
-					else {
-						var addWord = tradHashtable.get(wordList[i].traditional);
-						addWord.push(wordList[i]);
-						tradHashtable.put(wordList[i].traditional, addWord);
+
+					runPinyin();
+				}
+				if(tradIsEmpty == true) {
+					function runTraditional() {
+						'use strict'
+						let word = {
+							simplified: wordList[i].simplified,
+							traditional: wordList[i].traditional,
+							pronunciation: wordList[i].pronunciation,
+							definitions: wordList[i].definitions
+						};
+
+						// Prettify the pinyin to use tone marks instead of tone numbers
+						word.pronunciation = pinyin.prettify(word.pronunciation.substring(1, word.pronunciation.length - 1));
+
+						// Add the word to the hashtable, if the character already exists in the hashtable as a key, add the second definition to the same key
+						if(tradHashtable.containsKey(word.traditional) == false) {
+							let addWord = [];
+							addWord.push(word);
+							tradHashtable.put(word.traditional, addWord);
+						}
+						else {
+							let addWord = tradHashtable.get(word.traditional);
+							addWord.push(word);
+							tradHashtable.put(word.traditional, addWord);
+						}
 					}
+
+					runTraditional();
 				}
 				if(simpIsEmpty == true) {
-					/*
-					* 	TODO: Change pinyin (wordList.pronunciation) to use tone marks instead of tone numbers
-					*/
-					if(simpHashtable.containsKey(wordList[i].simplified) == false) {
-						var addWord = [];
-						addWord.push(wordList[i]);
-						simpHashtable.put(wordList[i].simplified, addWord);
+					function runSimplified() {
+						'use strict'
+						let word = {
+							simplified: wordList[i].simplified,
+							traditional: wordList[i].traditional,
+							pronunciation: wordList[i].pronunciation,
+							definitions: wordList[i].definitions
+						};
+
+						// Prettify the pinyin to use tone marks instead of tone numbers
+						word.pronunciation = pinyin.prettify(word.pronunciation.substring(1, word.pronunciation.length - 1));
+
+						// Add the word to the hashtable, if the character already exists in the hashtable as a key, add the second definition to the same key
+						if(simpHashtable.containsKey(word.simplified) == false) {
+							var addWord = [];
+							addWord.push(word);
+							simpHashtable.put(word.simplified, addWord);
+						}
+						else {
+							var addWord = simpHashtable.get(word.simplified);
+							addWord.push(word);
+							simpHashtable.put(word.simplified, addWord);
+						}
 					}
-					else {
-						var addWord = simpHashtable.get(wordList[i].simplified);
-						addWord.push(wordList[i]);
-						simpHashtable.put(wordList[i], addWord);
-					}
-				}
-				if(pinyinIsEmpty == true) {
-					/*
-					*	TODO: Make the pinyin that gets put into the hashtable searchable by removing '[', ']', and tone numbers
-					*/
-					if(pinyinHashtable.containsKey(wordList[i].pronunciation) == false) {
-						var addWord = [];
-						addWord.push(wordList[i]);
-						pinyinHashtable.put(wordList[i].pronunciation, addWord);
-					}
-					else {
-						var addWord = pinyinHashtable.get(wordList[i].pronunciation);
-						addWord.push(wordList[i]);
-						pinyinHashtable.put(wordList[i].pronunciation, addWord);
-					}
+
+					runSimplified();
 				}
 			}
 
@@ -227,59 +268,34 @@ module.exports.searchByChinese = function(str, cb) {
 
 // Search by pinyin
 /*
-*	TODO: Make pinyin searchable WITH AND WITHOUT tone numbers
-*		DESC: The input 'ni2 hao3' and 'ni hao' should both return results containing the character 你好
-*	TODO: Make pinyin searchable through hashtable
-*		DESC: The current search method (querying the database) has been DEPRECIATED, the updated search method employs hashtable for faster search.
-*				This might pose a particular issue for tone numbers, we might have to employ 2 hashtables, 1 that has tone numbers and 1 without,
-*				And then we are going to have to figure out if the which one the user is trying to search with.
+*	TODO: Make pinyin searchable WITH tone numbers
+*		DESC: The input 'ni2hao3' and 'nihao' should both return results containing the character 你好
 */
 module.exports.searchByPinyin = function(str, cb) {
-	//Catches dead-tones or 5th tones
-	var parts = str.split(" ");
-	var newStr = [];
-	_.each(parts, function(part) {
-		var numeric = part.replace(/\D/g,'');
+	// Convert querty to space separated array
+	str = str.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
+	str = str.replace(/[\$\uFFE5\^\+=`~<>{}\[\]|\u3000-\u303F!-#%-\x2A,-/:;\x3F@\x5B-\x5D_\x7B}\u00A1\u00A7\u00AB\u00B6\u00B7\u00BB\u00BF\u037E\u0387\u055A-\u055F\u0589\u058A\u05BE\u05C0\u05C3\u05C6\u05F3\u05F4\u0609\u060A\u060C\u060D\u061B\u061E\u061F\u066A-\u066D\u06D4\u0700-\u070D\u07F7-\u07F9\u0830-\u083E\u085E\u0964\u0965\u0970\u0AF0\u0DF4\u0E4F\u0E5A\u0E5B\u0F04-\u0F12\u0F14\u0F3A-\u0F3D\u0F85\u0FD0-\u0FD4\u0FD9\u0FDA\u104A-\u104F\u10FB\u1360-\u1368\u1400\u166D\u166E\u169B\u169C\u16EB-\u16ED\u1735\u1736\u17D4-\u17D6\u17D8-\u17DA\u1800-\u180A\u1944\u1945\u1A1E\u1A1F\u1AA0-\u1AA6\u1AA8-\u1AAD\u1B5A-\u1B60\u1BFC-\u1BFF\u1C3B-\u1C3F\u1C7E\u1C7F\u1CC0-\u1CC7\u1CD3\u2010-\u2027\u2030-\u2043\u2045-\u2051\u2053-\u205E\u207D\u207E\u208D\u208E\u2329\u232A\u2768-\u2775\u27C5\u27C6\u27E6-\u27EF\u2983-\u2998\u29D8-\u29DB\u29FC\u29FD\u2CF9-\u2CFC\u2CFE\u2CFF\u2D70\u2E00-\u2E2E\u2E30-\u2E3B\u3001-\u3003\u3008-\u3011\u3014-\u301F\u3030\u303D\u30A0\u30FB\uA4FE\uA4FF\uA60D-\uA60F\uA673\uA67E\uA6F2-\uA6F7\uA874-\uA877\uA8CE\uA8CF\uA8F8-\uA8FA\uA92E\uA92F\uA95F\uA9C1-\uA9CD\uA9DE\uA9DF\uAA5C-\uAA5F\uAADE\uAADF\uAAF0\uAAF1\uABEB\uFD3E\uFD3F\uFE10-\uFE19\uFE30-\uFE52\uFE54-\uFE61\uFE63\uFE68\uFE6A\uFE6B\uFF01-\uFF03\uFF05-\uFF0A\uFF0C-\uFF0F\uFF1A\uFF1B\uFF1F\uFF20\uFF3B-\uFF3D\uFF3F\uFF5B\uFF5D\uFF5F-\uFF65]+/g, "");
+	pinyin = str.split(" ");
 
-		if(numeric === "") {
-			part += "5";
-			newStr.push(part);
+	var results = [];
+	var i = 0;
+	var infiniteCheck = 0;
+	while(i < pinyin.length && infiniteCheck < 4000) {
+		if(pinyinHashtable.containsKey(pinyin[i])) {
+			results.push(pinyinHashtable.get(pinyin[i]));
 		}
 		else {
-			newStr.push(part);
+			console.log("Error: Could not find "+pinyin[i]+" in the database.");
+			/*
+			* TODO: Report this error to the user.
+			*/
 		}
-	});
 
-	str = "[" + newStr.join(" ") + "]";
+		infiniteCheck++;
+		i++;
+	}
 
-	// Consider using the $where query option
-	var query = {
-		pronunciation: str
-	};
-
-	cedict.find(query).toArray(function(err, words) {
-		if(err || words == undefined || words == null) {
-			console.log("There was an error while querying the database.");
-			console.log(err);
-			// TODO: Report error to the user.
-			cb([]);
-		}
-		else {
-			var results = [];
-			_.each(words, function(word) {
-				var pronunciation = word.pronunciation;
-				var prettified = pinyin.prettify(pronunciation.slice(1, pronunciation.length - 1));
-				results.push({
-					traditional: word.traditional,
-					simplified: word.simplified,
-					pronunciation: prettified,
-					definitions: word.definitions
-				});
-			});
-
-			cb(results);
-		}
-	});
+	cb(results);
 };
 
 /*
