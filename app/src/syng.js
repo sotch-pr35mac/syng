806e7e1676e053c03c6b0b96492cc24b4333266f
Preston Stosur-Bassett <preston@stosur.info>
2017-12-19 23:44:20 -0500

    updated to latest prettify-pinyin release

--- app/src/syng.js
+++ app/src/syng.js
@@ -26846,7 +26846,13 @@ var replacements = {
   'u': ['ū', 'ú', 'ǔ', 'ù'],
   'i': ['ī', 'í', 'ǐ', 'ì'],
   'o': ['ō', 'ó', 'ǒ', 'ò'],
-  'ü': ['ǖ', 'ǘ', 'ǚ', 'ǜ']
+  'ü': ['ǖ', 'ǘ', 'ǚ', 'ǜ'],
+  'A': ['Ā', 'Á', 'Ă', 'À'],
+  'E': ['Ē', 'É', 'Ĕ', 'È'],
+  'U': ['Ū', 'Ú', 'Ŭ', 'Ù'],
+  'I': ['Ī', 'Í', 'Ĭ', 'Ì'],
+  'O': ['Ō', 'Ó', 'Ŏ', 'Ò'],
+  'Ü': ['Ǖ', 'Ǘ', 'Ǚ', 'Ǜ']
 };
 
 var medials = ['i', 'u', 'ü'];
@@ -26863,7 +26869,7 @@ var prettify = function(str){
     if(isNaN(tone)) {
       continue;
     }
-    
+
     if (tone <= 0 || tone > 5) {
       console.error('invalid tone number:', tone, 'in', syllable);
     } else if (tone === 5){
@@ -26889,7 +26895,7 @@ var prettify = function(str){
           syllables[i] = replaced.slice(0, replaced.length - 1);
           break;
         }
-      }  
+      }
     }
 
   }
@@ -26897,8 +26903,6 @@ var prettify = function(str){
 };
 
 module.exports.prettify = prettify;
-
-
 
 },{}],42:[function(require,module,exports){
 // shim for using process in browser
