8b5cd7803b120eba2c9e995156df5923dd531cb5
Preston Wang-Stosur-Bassett <preston@stosur.info>
2018-02-02 10:32:25 -0500

    added framework for settings page

--- app/src/component/syngMenu/syngMenu.vue
+++ app/src/component/syngMenu/syngMenu.vue
@@ -6,14 +6,11 @@
           <h2>About</h2>
         </center>
       </Menu-item>
-      <!-- Settings Menu not implimented in this version -->
-      <!--
       <Menu-item key="2" v-on:click='loadView("settings")'>
         <center>
           <h2>Settings</h2>
         </center>
       </Menu-item>
-      -->
       <Menu-item key="3" v-on:click='loadView("viewLicense")'>
         <center>
           <h2>View Licenses</h2>
