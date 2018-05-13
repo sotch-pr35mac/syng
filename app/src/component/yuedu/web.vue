3c9c1d9373c98dd7aa50c92fe34d1f7a975a62ae
Preston Wang-Stosur-Bassett <p.wanstobas@gmail.com>
2018-05-13 16:15:15 -0400

    built out more of the reader feature

---
+++ app/src/component/yuedu/web.vue
@@ -0,0 +1,20 @@
+<template>
+	<div>
+		<center>
+			<h1>WEB</h1>
+		</center>
+	</div>
+</template>
+
+<style scoped>
+</style>
+
+<script>
+module.exports = {
+	data: function() {
+		return {
+
+		}
+	}
+}
+</script>
--- app/src/component/yuedu/yuedu.vue
+++ app/src/component/yuedu/yuedu.vue
@@ -1,14 +1,46 @@
 <template>
   <div>
-    <component :is="currentView"></component>
+    <i-col span="21">
+      <div id="actions-frame">
+        <Row>
+          <i-button v-on:click="addToLibrary()" id="addToLibraryButton" class="pull-right">Add to Library</i-button>
+        </Row>
+      </div>
+      <br>
+      <center>
+        <Button-group>
+          <i-button v-if="currentView == 'library'" size="large" type="primary">Library</i-button>
+          <i-button v-if="currentView != 'library'" size="large" v-on:click="switchMode('library')">Library</i-button>
+          <i-button v-if="currentView == 'clipboard'" size="large" type="primary">Clipboard</i-button>
+          <i-button v-if="currentView != 'clipboard'" size="large" v-on:click="switchMode('clipboard')">Clipboard</i-button>
+          <i-button v-if="currentView == 'web'" size="large" type="primary">Web</i-button>
+          <i-button v-if="currentView != 'web'" size="large" v-on:click="switchMode('web')">Web</i-button>
+        </Button-group>
+      </center>
+      <br>
+      <component :is="currentView"></component>
+    </i-col>
   </div>
 </template>
 
 <style>
+  #actions-frame {
+    padding-left: 10px;
+    padding-right: 10px;
+    padding-top: 10px;
+    padding-bottom: 10px;
+    background-color: #9ea7b4;
+    height: 8vh;
+  }
+  .pull-right {
+    float: right;
+  }
 </style>
 
 <script>
 var Library = require('./library.vue');
+var Clipboard = require('./clipboard.vue');
+var Web = require('./web.vue');
 var Reader = require('./reader.vue');
 
 module.exports = {
@@ -18,7 +50,18 @@ module.exports = {
     }
   },
   components: {
-    'library': Library
+    'library': Library,
+    'web': Web,
+    'clipboard': Clipboard,
+    'reader': Reader
+  },
+  methods: {
+    addToLibrary: function() {
+      alert("This feature is not yet supported.");
+    },
+    switchMode(view) {
+      this.currentView = view;
+    }
   }
 }
 </script>
