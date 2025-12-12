537e1ad45ea4005bfa2f42df9e384c42357c3d32
Preston Wang-Stosur-Bassett <p.wanstobas@gmail.com>
2025-12-12 20:22:59 +0800

    Updated JS packages

--- src/views/templates/components/SyToggle/SyToggle.svelte
+++ src/views/templates/components/SyToggle/SyToggle.svelte
@@ -1,6 +1,5 @@
 <script>
-import { createEventDispatcher } from 'svelte';
-import { v4 as uuid } from 'uuid';
+import { createEventDispatcher } from "svelte";
 
 /* Checked Prop */
 /* Possible Values */
@@ -9,8 +8,8 @@ import { v4 as uuid } from 'uuid';
 export let checked = false;
 
 /* ID Prop */
-// HTML ID for reference 
-export let id = uuid();
+// HTML ID for reference
+export let id = crypto.randomUUID();
 
 /* Value Prop */
 // The value property for the HTML checkbox
@@ -19,49 +18,56 @@ export let value;
 const dispatch = createEventDispatcher();
 </script>
 
+<span class="sy-toggle--container">
+  <input
+    type="checkbox"
+    {id}
+    class="sy-toggle"
+    {value}
+    {checked}
+    on:change={(e) => dispatch("change", e.srcElement.checked)}
+  />
+  <label for={id} class="sy-toggle--label">Toggle</label>
+</span>
+
 <style>
 .sy-toggle--container {
-	display: flex;
+  display: flex;
 }
 .sy-toggle {
-	height: 0;
-	width: 0;
-	visibility: hidden;
+  height: 0;
+  width: 0;
+  visibility: hidden;
 }
 .sy-toggle:checked + .sy-toggle--label {
-	background: var(--sy-color--blue);
+  background: var(--sy-color--blue);
 }
 .sy-toggle:checked + .sy-toggle--label:after {
-	left: calc(100% - 1.5625px);
-	transform: translateX(-100%);
+  left: calc(100% - 1.5625px);
+  transform: translateX(-100%);
 }
 .sy-toggle--label {
-	cursor: pointer;
-	text-indent: -9999px;
-	width: 62.5px;
-	height: 31.125px;
-	border-radius: 31.125px;
-	background: var(--sy-color--grey-4); 
-	display: block;
-	position: relative;
+  cursor: pointer;
+  text-indent: -9999px;
+  width: 62.5px;
+  height: 31.125px;
+  border-radius: 31.125px;
+  background: var(--sy-color--grey-4);
+  display: block;
+  position: relative;
 }
 .sy-toggle--label:after {
-	content: '';
-	position: absolute;
-	top: 1.5625px;
-	left: 1.5625px;
-	width: 28.125px;
-	height: 28.125px;
-	border-radius: 28.125px;
-	background: var(--sy-color--white);
-	transition: var(--sy-transition-duration);
+  content: "";
+  position: absolute;
+  top: 1.5625px;
+  left: 1.5625px;
+  width: 28.125px;
+  height: 28.125px;
+  border-radius: 28.125px;
+  background: var(--sy-color--white);
+  transition: var(--sy-transition-duration);
 }
 .sy-toggle--label:active:after {
-	width: 40.625px;;
+  width: 40.625px;
 }
 </style>
-
-<span class="sy-toggle--container">
-	<input type="checkbox" id="{ id }" class="sy-toggle" value="{ value }" checked="{ checked }" on:change={ e => dispatch('change', e.srcElement.checked) } />
-	<label for="{ id }" class="sy-toggle--label">Toggle</label>
-</span>
